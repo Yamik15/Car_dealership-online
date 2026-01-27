51be95d823776f0bd064e1027c48f518861c75fe
Anya <ankakotik74@gmail.com>
2026-01-27 23:01:36 +0400

    Главная страница без карточек и без инициал в кнопке личного кабинета

---
+++ Car_dealership_online/frontend/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header/Header';
+import './global.css'
+import DreamDeliverySection from './components/DreamDeliverySection/DreamDeliverySection';
+import FormSection from './components/FormSection/FormSection';
+import ButtonsSection from './components/ButtonsSection/ButtonsSection'
+import Footer from './components/Footer/Footer';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header />
+      <DreamDeliverySection />
+      <FormSection/>
+      <ButtonsSection/>
+      <Footer/>
+    </div>
+  )
+}
